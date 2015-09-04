5025100f582122d40446da0b1e529367ea16cc17
Rodrigo Fuentes <rodrigo.fuentes@kinnser.com>
2015-09-04 17:08:19 -0300

    Selected this month period before filling the timecard.

--- steps/adp.gotoMyTimecard.js
+++ steps/adp.gotoMyTimecard.js
@@ -1,3 +1,55 @@
+casper.adp._changePeriod = function ( periodText ) {
+    var eventFire = function eventFire(el, etype){
+      if (el.fireEvent) {
+       (el.fireEvent('on' + etype));
+      } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+      }
+    }
+    
+    var output = [];
+    try {
+        var periodSelector = document.querySelectorAll("[id='ctrlDtRangeSelector_SelectionItem']")[0];
+
+        eventFire(periodSelector, 'mouseover');
+        eventFire(periodSelector, 'click');
+        var newOption, oldOption;
+
+        oldOption = periodSelector.options[ periodSelector.selectedIndex ];
+        newOption = oldOption.text == periodText ? newOption = oldOption : null;
+
+        for ( var i = 0 ; i < periodSelector.options.length && !newOption; i++){
+            if( periodSelector.options[i].text == periodText){
+                newOption = periodSelector.options[i];
+            };
+        };  
+
+        if( newOption ){
+            periodSelector.selectedIndex = newOption.index;
+            eventFire(periodSelector, 'change');
+        }
+
+        output.push({
+            'oldOption' : {
+                'index': oldOption.index,
+                'value': oldOption.value,
+                'text': oldOption.text
+            },
+            'newOption' : {
+                'index': newOption.index,
+                'value': newOption.value,
+                'text': newOption.text
+            }
+        });
+
+    } catch( e ) {
+        output.push( e );
+    }
+
+    return output;
+}
 
 casper.adp.gotoMyTimecard = function (  ) {
 
@@ -23,8 +75,25 @@ casper.adp.gotoMyTimecard = function (  ) {
 
 casper.adp.thisMonthTimecard = function (  ) {
 
+
+    casper.withFrame('IFRM', function() {
+        
+        config.saveStepsScreenshots && this.capture( './screenshots/' + now + '.4.1.LandingMyTimecard.png');
+
+        var result = this.evaluate( casper.adp._changePeriod, "This Month"); 
+
+        if( result && result.length && result[0].newOption && result[0].oldOption ){
+            newOption = result[0].newOption;
+            oldOption = result[0].oldOption;
+            this.echo("Previous period: (" + oldOption.value + ") " + oldOption.text);
+            this.echo("Changed to period: (" + newOption.value + ") " + newOption.text);
+        }
+
+    });
+
     casper.then( function() {
-        this.echo('4. Landing this month timecard at '  + this.getCurrentUrl());
-        config.saveStepsScreenshots && this.capture( './screenshots/' + now + '.4.LandingThisMonthTimecard.png');
+
+        this.echo('4. Opened this month timecard at '  + this.getCurrentUrl());
+        config.saveStepsScreenshots && this.capture( './screenshots/' + now + '.4.2.OpenedThisMonthPeriod.png');
     });
 };
